7fd8fa6ac9bbf1b2b855ad50f7751635c21d8770
Bogdan Covrig <bogcov@gmail.com>
2021-12-09 18:20:27 +0100

    Add reaction grapql query

--- generate-codeowners.ts
+++ generate-codeowners.ts
@@ -6,17 +6,36 @@ const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 const [refs, pull, prNumber, merge] = process.env.GITHUB_REF.split("/");
 
 const run = async () => {
-  //   const response = await octokit.graphql(
-  //     `query ($login: String!) {
-  //           organization(login: $login) {
-  //             repositories() {
-  //               totalCount
-  //             }
-  //           }
-  //         }`
-  //   );
-  //   console.log(response);
+  const response = await octokit.graphql(
+    `
+    query ($owner: String!, $name: String!, $prNumber: Int!) {
+        repository(owner: $owner, name: $name) {
+          pullRequest(number: $prNumber) {
+            comments(first: 100) {
+              edges {
+                node {
+                  reactions(last: 100) {
+                    nodes {
+                      content
+                      user {
+                        login
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`,
+    {
+      owner,
+      name: repo,
+      prNumber,
+    }
+  );
 
+  console.log(response);
   console.log(owner, repo, refs, pull, prNumber, merge);
 };
 
