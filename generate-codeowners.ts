653ad935b4fff3020949aa3d74988f2e0683c1d3
Bogdan Covrig <bogcov@gmail.com>
2021-12-09 17:03:12 +0100

    Add octokit graphql

--- generate-codeowners.ts
+++ generate-codeowners.ts
@@ -2,3 +2,13 @@ import { Octokit } from "@octokit/action";
 
 const octokit = new Octokit();
 console.log(octokit);
+
+const response = await octokit.graphql(
+  `query ($login: String!) {
+    organization(login: $login) {
+      repositories() {
+        totalCount
+      }
+    }
+  }`
+);
